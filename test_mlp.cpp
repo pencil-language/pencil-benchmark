41b57f1d2f1541a4949ba699311d562b85a6fbb6
Laszlo Marak <laszlo.marak@realeyesit.com>
2013-02-26 16:36:48 +0100

    mlp testing executable

---
+++ test_mlp.cpp
@@ -0,0 +1,9 @@
+// UjoImro, 2013
+// Experimental code for the CARP Project
+// Copyright (c) RealEyes, 2013
+// This version tests the responseMap calculation with input dumps
+
+
+
+
+// LuM end of file
