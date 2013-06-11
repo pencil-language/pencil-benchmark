f244b883073b3741eab44c32de51f0b208faae51
Róbert Dávid <robert.david@realeyesit.com>
2013-06-11 15:15:20 +0200

    Port parallelized calls and IPP/MKL calls from GEL

--- mlp/gel_mlp.cpp
+++ mlp/gel_mlp.cpp
@@ -3,22 +3,19 @@
 // Copyright (c) RealEyes, 2013
 // This version tests the responseMap calculation with input dumps
 
-
-/*
-  extern int EF_ALIGNMENT = 0;
-  extern int EF_PROTECT_BELOW = 0;
-  extern int EF_PROTECT_FREE = 0;
-  extern int EF_ALLOW_MALLOC_0 = 1;
-  extern int EF_FILL = 1922;
-*/
-
 #include <boost/smart_ptr.hpp>
 
 #include "opencl.hpp"
 #include "memory.hpp"
 #include "bench_mlp.hpp"
 
-const int processed_frames = 100;
+#include <tbb/parallel_for.h>
+#include <tbb/concurrent_vector.h>
+#include <tbb/tbb_thread.h>
+#include <tbb/task_scheduler_init.h>
+
+
+const int processed_frames = 10;
 
 int main()
 {
@@ -28,7 +25,11 @@ int main()
     int64_t maxnetallocated = 0;
     int64_t maxgrossallocated = 0;
     
-    
+    int numberOfThreads =  tbb::tbb_thread::hardware_concurrency();
+    PRINT(numberOfThreads);    
+    tbb::task_scheduler_init init(numberOfThreads);
+
+
     for ( conductor.importer >> BOOST_SERIALIZATION_NVP(conductor.id);
           ((conductor.id != -1) && (conductor.id != processed_frames));
           // conductor.id != -1;
@@ -41,14 +42,13 @@ int main()
         // here comes the function call
         {           
             gel::MLP<double> mlp;
-            std::vector< cv::Mat_<double> > calculatedResults;
+            tbb::concurrent_vector< cv::Mat_<double> > calculatedResults(conductor.hack.m_visibleLandmarks_size);
             auto start = std::chrono::high_resolution_clock::now();
-            for (int q=0; q<conductor.hack.m_visibleLandmarks_size; q++ )
-            {
+            tbb::parallel_for(0,conductor.hack.m_visibleLandmarks_size,[&](int q){
+//            for(int q = 0; q < conductor.hack.m_visibleLandmarks_size; ++q) {
                 const cv::Point2i center(cvRound(conductor.hack.shape(2*q,0)),cvRound(conductor.hack.shape(2*q+1,0)));
-                auto nextResult =conductor.hack.m_classifiers[q].generateResponseMap(conductor.hack.alignedImage, center, conductor.hack.m_mapSize);
-                calculatedResults.push_back(nextResult);
-            }
+                calculatedResults[q] = conductor.hack.m_classifiers[q].generateResponseMap(conductor.hack.alignedImage, center, conductor.hack.m_mapSize);
+            });
             auto end = std::chrono::high_resolution_clock::now();
             elapsed_time += microseconds(end - start);
 
