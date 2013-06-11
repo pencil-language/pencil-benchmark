668d60ef6f54bf54ef7f10df58cbf9d4114af8c2
Laszlo Marak <laszlo.marak@realeyesit.com>
2013-06-11 10:38:20 +0200

    compilation fixed for the Intel(tm) Compiler on Linux

---
+++ cvtColor/test_color.cpp
@@ -0,0 +1,42 @@
+// UjoImro, 2013
+// Experimental code for the CARP Project
+// Copyright (c) RealEyes, 2013
+
+
+
+int main()
+{
+    
+} // main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LuM end of file
