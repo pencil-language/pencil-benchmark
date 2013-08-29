4c450889b19b7811ab538465739a21581e13ef04
Laszlo Marak <laszlo.marak@realeyesit.com>
2013-08-29 17:27:32 +0200

    refactoring before pencilization
    Former-commit-id: a022b6ff69bd9003b6c0f1293c6f55853f3eb3c0

--- filter2D/test_filter2D.cpp
+++ filter2D/test_filter2D.cpp
@@ -17,6 +17,59 @@ namespace {
     }
 } // unnamed namespace
 
+namespace carp {
+    
+    cv::ocl::oclMat
+    filter2D( carp::opencl::device & device, cv::ocl::oclMat gpu_gray, cv::ocl::oclMat kernel_gpu, int border_type ) {
+        CV_Assert(gpu_gray.depth() == CV_32F);
+        CV_Assert(kernel_gpu.depth() == CV_32F);
+        cv::ocl::oclMat gpu_convolve;        
+        gpu_convolve.create(gpu_gray.size(), gpu_gray.type());
+        CV_Assert(gpu_gray.type() == kernel_gpu.type() && gpu_gray.size() == gpu_convolve.size());
+
+        std::string kernelName = "convolve_D5";
+
+        CV_Assert(gpu_gray.depth() == CV_32FC1);
+        CV_Assert(kernel_gpu.depth() == CV_32F);
+        CV_Assert(kernel_gpu.cols <= 17 && kernel_gpu.rows <= 17);
+
+        gpu_convolve.create(gpu_gray.size(), gpu_gray.type());
+
+        CV_Assert(gpu_gray.cols == gpu_convolve.cols && gpu_gray.rows == gpu_convolve.rows);
+        CV_Assert(gpu_gray.type() == gpu_convolve.type());
+
+        cv::ocl::Context  *clCxt = gpu_gray.clCxt;
+        int channels = gpu_convolve.oclchannels();
+        int depth = gpu_convolve.depth();
+
+        size_t vector_length = 1;
+        int offset_cols = ((gpu_convolve.offset % gpu_convolve.step) / gpu_convolve.elemSize1()) & (vector_length - 1);
+        int cols = divUp(gpu_convolve.cols * channels + offset_cols, vector_length);
+        int rows = gpu_convolve.rows;
+
+        std::vector<size_t> localThreads  = { 16, 16, 1 };
+        std::vector<size_t> globalThreads = { divUp(cols, localThreads[0]) *localThreads[0],
+                                              divUp(rows, localThreads[1]) *localThreads[1],
+                                              1 };
+                
+        device[kernelName] (
+            reinterpret_cast<cl_mem>(gpu_gray.data),
+            reinterpret_cast<cl_mem>(kernel_gpu.data),
+            reinterpret_cast<cl_mem>(gpu_convolve.data),
+            gpu_gray.rows,
+            cols,
+            static_cast<int>(gpu_gray.step),
+            static_cast<int>(gpu_convolve.step),
+            static_cast<int>(kernel_gpu.step),
+            kernel_gpu.rows,
+            kernel_gpu.cols
+            ).groupsize( localThreads, globalThreads );
+
+        return gpu_convolve;
+    } // filter2D
+    
+} // namespace carp
+
 
 template<class T0>
 void
@@ -52,58 +105,14 @@ time_filter2D( carp::opencl::device & device, T0 & pool, int iteration )
             const auto cpu_end = std::chrono::high_resolution_clock::now();
             elapsed_time_cpu += carp::microseconds(cpu_end - cpu_start);
 
-            {
-                cv::ocl::oclMat gpu_gray(cpu_gray);
-                cv::ocl::oclMat gpu_convolve;
-                cv::ocl::oclMat kernel_gpu(kernel_cpu);                
-                
-                CV_Assert(gpu_gray.depth() == CV_32F);
-                CV_Assert(kernel_gpu.depth() == CV_32F);
-                gpu_convolve.create(gpu_gray.size(), gpu_gray.type());
-                CV_Assert(gpu_gray.type() == kernel_gpu.type() && gpu_gray.size() == gpu_convolve.size());
-
-                std::string kernelName = "convolve_D5";
-
-                CV_Assert(gpu_gray.depth() == CV_32FC1);
-                CV_Assert(kernel_gpu.depth() == CV_32F);
-                CV_Assert(kernel_gpu.cols <= 17 && kernel_gpu.rows <= 17);
-
-                gpu_convolve.create(gpu_gray.size(), gpu_gray.type());
-
-                CV_Assert(gpu_gray.cols == gpu_convolve.cols && gpu_gray.rows == gpu_convolve.rows);
-                CV_Assert(gpu_gray.type() == gpu_convolve.type());
-
-                cv::ocl::Context  *clCxt = gpu_gray.clCxt;
-                int channels = gpu_convolve.oclchannels();
-                int depth = gpu_convolve.depth();
-
-                size_t vector_length = 1;
-                int offset_cols = ((gpu_convolve.offset % gpu_convolve.step) / gpu_convolve.elemSize1()) & (vector_length - 1);
-                int cols = divUp(gpu_convolve.cols * channels + offset_cols, vector_length);
-                int rows = gpu_convolve.rows;
-
-                std::vector<size_t> localThreads  = { 16, 16, 1 };
-                std::vector<size_t> globalThreads = { divUp(cols, localThreads[0]) *localThreads[0],
-                                            divUp(rows, localThreads[1]) *localThreads[1],
-                                                      1 };
-                
-                auto gpu_start = std::chrono::high_resolution_clock::now();
-                device[kernelName] (
-                    reinterpret_cast<cl_mem>(gpu_gray.data),
-                    reinterpret_cast<cl_mem>(kernel_gpu.data),
-                    reinterpret_cast<cl_mem>(gpu_convolve.data),
-                    gpu_gray.rows,
-                    cols,
-                    static_cast<int>(gpu_gray.step),
-                    static_cast<int>(gpu_convolve.step),
-                    static_cast<int>(kernel_gpu.step),
-                    kernel_gpu.rows,
-                    kernel_gpu.cols
-                    ).groupsize( localThreads, globalThreads );
-                auto gpu_end = std::chrono::high_resolution_clock::now();
-                elapsed_time_gpu += carp::microseconds(gpu_end - gpu_start);
-                check = gpu_convolve;
-            }
+            cv::ocl::oclMat gpu_convolve;            
+            cv::ocl::oclMat gpu_gray(cpu_gray);
+            cv::ocl::oclMat kernel_gpu(kernel_cpu);
+            const auto gpu_start = std::chrono::high_resolution_clock::now();
+            gpu_convolve = carp::filter2D( device, gpu_gray, kernel_gpu, cv::BORDER_REPLICATE );
+            const auto gpu_end = std::chrono::high_resolution_clock::now();
+            elapsed_time_gpu += carp::microseconds(gpu_end - gpu_start);
+            check = gpu_convolve;            
             
             // Verifying the results
             if ( cv::norm(host_convolve - check) > 0.01 ) {
