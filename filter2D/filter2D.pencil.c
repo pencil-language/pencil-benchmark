ed929e7c8793c746b69b934f0d0052a096ba5195
Laszlo Marak <laszlo.marak@realeyesit.com>
2013-08-30 13:25:24 +0200

    pencilized filter2D
    Former-commit-id: 056cc50beb85764ff86a30062642e36dc56e64b9

--- filter2D/filter2D.pencil.c
+++ filter2D/filter2D.pencil.c
@@ -8,6 +8,46 @@
 
 #include "filter2D.pencil.h"
 
+static void 
+filter2D(     
+    int rows,
+    int cols,
+    int src_step,
+    const float src[static const restrict src_step][cols],
+    int kernel_rows,
+    int kernel_cols,
+    int kernel_step,
+    const float kernel[static const restrict kernel_step][kernel_cols],
+    int conv_step, 
+    float conv[static const restrict conv_step][cols] ) {
+    
+    int center_row = kernel_rows / 2;
+    int center_col = kernel_cols / 2;
+   
+#   pragma independent
+    for ( int q = 0; q < rows; q++ ) 
+#       pragma independent
+     	for ( int w = 0; w < cols; w++ ) { 
+     	    float prod = 0.;
+#           pragma reduction sum on prod
+     	    for ( int e = 0; e < kernel_rows; e++ )
+     	    	for ( int r = 0; r < kernel_cols; r++ )
+	    	{
+	    	    int row = q + e - center_row;
+     	    	    int col = w + r - center_col;
+		    row = row < 0 ? 0 : row;
+		    row = row < rows ? row : rows - 1;
+		    col = col < 0 ? 0 : col;
+		    col = col < cols ? col : cols - 1;
+	    	    // if ( (row>=0) && (row<rows) && (col>=0) && (col<cols) )
+		    prod += src[row][col] * kernel[e][r];
+     	    	}
+	    conv[q][w] = prod;
+     	}
+    
+    return;
+} // filter2D 
+
 void 
 pencil_filter2D( 
     int rows,
@@ -20,27 +60,8 @@ pencil_filter2D(
     float kernel[],
     int conv_step, 
     float conv[] ) {
-
-    int center_row = kernel_rows / 2;
-    int center_col = kernel_cols / 2;
-   
-     for ( int q = 0; q < rows; q++ ) 
-     	for ( int w = 0; w < cols; w++ ) { 
-     	    float prod = 0.;
-     	    for ( int e = 0; e < kernel_rows; e++ )
-     	    	for ( int r = 0; r < kernel_cols; r++ )
-	    	{
-	    	    int row = q + e - center_row;
-     	    	    int col = w + r - center_col;
-		    row = row < 0 ? 0 : row;
-		    row = row < rows ? row : rows - 1;
-		    col = col < 0 ? 0 : col;
-		    col = col < cols ? col : cols - 1;
-	    	    // if ( (row>=0) && (row<rows) && (col>=0) && (col<cols) )
-		    prod += src[ row * src_step + col ] * kernel[ e * kernel_step + r ];
-     	    	}
-	    conv[ q * conv_step + w ] = prod;
-     	} 
+    
+    filter2D( rows, cols, src_step, src, kernel_rows, kernel_cols, kernel_step, kernel, conv_step, conv );
 
     return;
 } // filter2D
