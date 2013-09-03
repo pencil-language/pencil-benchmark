80b0bef54c2d1b6a25f2437c35dd4dc3437b931c
Laszlo Marak <laszlo.marak@realeyesit.com>
2013-09-03 16:23:02 +0200

    pencilization of the resize operator finished
    Former-commit-id: 6a02c744b4b10d8cdbe71edd9fda25afae74de85

--- resize/resize.pencil.c
+++ resize/resize.pencil.c
@@ -39,26 +39,27 @@ sat( int val, int lo, int hi ) {
     return val;
 }
 
-void
-pencil_resize_LN (
+static void
+resize (
     int original_rows,
     int original_cols,
     int original_step,
-    uint8_t original[],
+    uint8_t original[static const restrict original_step][original_cols],
     int resampled_rows,
     int resampled_cols,
     int resampled_step,
-    uint8_t resampled[] ) {
+    uint8_t resampled[static const restrict resampled_step][resampled_cols] ) {
 
     assert(resampled_rows>1);
     assert(resampled_cols>1);
 
-    float o_h = original_rows; 
-    float o_w = original_cols; 
-    float n_h = resampled_rows; 
-    float n_w = resampled_cols; 
-
+    float o_h = original_rows;
+    float o_w = original_cols;
+    float n_h = resampled_rows;
+    float n_w = resampled_cols;
+#   pragma independent
     for ( int n_r = 0; n_r < resampled_rows; n_r++ )
+#       pragma independent
      	for ( int n_c = 0; n_c < resampled_cols; n_c++ ) {
 	    float o_r = ( n_r + 0.5 ) * (o_h) / (n_h) - 0.5;
  	    float o_c = ( n_c + 0.5 ) * (o_w) / (n_w) - 0.5;
@@ -78,17 +79,25 @@ pencil_resize_LN (
  	    int coord_11_r = sat( coord_00_r + 1, 0, o_h - 1 );
      	    int coord_11_c = sat( coord_00_c + 1, 0, o_w - 1 );
 	    
-	    uint8_t A00 = original[ coord_00_r * original_step + coord_00_c ];
-	    uint8_t A10 = original[ coord_10_r * original_step + coord_10_c ];
-	    uint8_t A01 = original[ coord_01_r * original_step + coord_01_c ];
-	    uint8_t A11 = original[ coord_11_r * original_step + coord_11_c ];
+	    uint8_t A00 = original[coord_00_r][coord_00_c];
+	    uint8_t A10 = original[coord_10_r][coord_10_c];
+	    uint8_t A01 = original[coord_01_r][coord_01_c];
+	    uint8_t A11 = original[coord_11_r][coord_11_c];
 	    
-	    resampled[ (n_r) * resampled_step + (n_c) ] = bilinear( A00, A01, A11, A10, r, c );
+	    resampled[n_r][n_c] = bilinear( A00, A01, A11, A10, r, c );
      	} // for n_c, n_r
 
     return;
 } // pencil_resize_LN
 
+void
+pencil_resize_LN (
+    int original_rows,  int original_cols,  int original_step,  uint8_t original[],
+    int resampled_rows, int resampled_cols, int resampled_step, uint8_t resampled[] ) {
+
+    resize( original_rows, original_cols, original_step, original, 
+	    resampled_rows, resampled_cols, resampled_step, resampled );
+} // pencil_resize_LN
 
 
 // LuM end of file
