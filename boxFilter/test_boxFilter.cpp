02f58f9066fda8eb72b47022c2a85ab9cf1fe9dc
Róbert Dávid <robert.david@realeyesit.com>
2014-03-12 18:21:44 +0100

    Refactor boxfilter and cvIntegral to use the same functions as pencil-enabled tests
    Former-commit-id: ec499daceee544c2453f8dbe75832295be3e1ad2

--- boxFilter/test_boxFilter.cpp
+++ boxFilter/test_boxFilter.cpp
@@ -29,123 +29,102 @@ namespace
 
 } // unnamed namespace
 
-    
+
 
 template<class T0>
 void
-time_boxFilter( carp::opencl::device & device, T0 & pool )
+time_boxFilter( T0 & pool )
 {
     std::vector<int> ksizes = { 11,15,21,25,31,35,41 };
     std::vector<std::pair<std::string, int> > border_types =
         { {"BORDER_REPLICATE",   cv::BORDER_REPLICATE },
           {"BORDER_REFLECT",     cv::BORDER_REFLECT },
           {"BORDER_REFLECT_101", cv::BORDER_REFLECT_101 },
-//          {"BORDER_WRAP",        cv::BORDER_WRAP },
           {"BORDER_CONSTANT",    cv::BORDER_CONSTANT } };
-    
-    double sum_quotient = 0;
 
-    carp::TimingShort timing;
-        
+    carp::TimingLong timing;
+
     for ( auto & item : pool ) {
 
         PRINT(item.path());
         for ( auto size : ksizes ) {
 
-            PRINT(size);        
-            long int elapsed_time_gpu = 0;
-            long int elapsed_time_cpu = 0;
+            PRINT(size);
+            std::chrono::microseconds elapsed_time_cpu(0), elapsed_time_gpu(0), elapsed_time_pencil(999999);
 
             for ( auto & border_type : border_types ) {
-                // PRINT(border_type.first);            
+                // PRINT(border_type.first);
 
-                cv::Mat host_filtered;
                 cv::Mat cpu_gray;
-                cv::Mat check;    
+                cv::cvtColor( item.cpuimg(), cpu_gray, CV_RGB2GRAY );
+
+                cv::Mat cpu_result, gpu_result;
 
                 // the ksize must be 2k+1 form and anchor must be (2k+1)<<1
                 cv::Size  ksize(size,size);
                 cv::Point anchor((size>>1),(size>>1));
-        
-                cv::cvtColor( item.cpuimg(), cpu_gray, CV_RGB2GRAY );
-                auto cpu_start = std::chrono::high_resolution_clock::now();
-                cv::boxFilter( cpu_gray, host_filtered, -1, ksize, anchor, true, border_type.second );
-                auto cpu_end = std::chrono::high_resolution_clock::now();
-                elapsed_time_cpu += carp::microseconds(cpu_end - cpu_start);
 
-                auto gpu_start = std::chrono::high_resolution_clock::now();
-                cv::ocl::oclMat gpu_result;
-                cv::ocl::oclMat gpu_gray(cpu_gray);
-                gpu_result.create( cpu_gray.rows, cpu_gray.cols, CV_8U );
-                float alpha = ksize.height * ksize.width;
-                size_t blockSizeX = 256, blockSizeY = 1;
-                size_t gSize = blockSizeX - (ksize.width - 1);
-                size_t threads = (gpu_result.offset % gpu_result.step % 4 + gpu_result.cols + 3) / 4;
-                size_t globalSizeX = threads % gSize == 0 ? threads / gSize * blockSizeX : (threads / gSize + 1) * blockSizeX;
-                size_t globalSizeY = ((gpu_result.rows + 1) / 2) % blockSizeY == 0 ? ((gpu_result.rows + 1) / 2) : (((gpu_result.rows + 1) / 2) / blockSizeY + 1) * blockSizeY;
 
-                normalizeAnchor(anchor, ksize);
+                {
+                    auto cpu_start = std::chrono::high_resolution_clock::now();
+                    cv::boxFilter( cpu_gray, cpu_result, -1, ksize, anchor, true, border_type.second );
+                    auto cpu_end = std::chrono::high_resolution_clock::now();
+                    elapsed_time_cpu += (cpu_end - cpu_start);
+                }
+                {
+                    float alpha = ksize.height * ksize.width;
+                    size_t blockSizeX = 256, blockSizeY = 1;
+                    size_t gSize = blockSizeX - (ksize.width - 1);
+                    size_t threads = (gpu_result.offset % gpu_result.step % 4 + gpu_result.cols + 3) / 4;
+                    size_t globalSizeX = threads % gSize == 0 ? threads / gSize * blockSizeX : (threads / gSize + 1) * blockSizeX;
+                    size_t globalSizeY = ((gpu_result.rows + 1) / 2) % blockSizeY == 0 ? ((gpu_result.rows + 1) / 2) : (((gpu_result.rows + 1) / 2) / blockSizeY + 1) * blockSizeY;
 
-                device.source_compile(
-                    filtering_boxFilter_cl,
-                    filtering_boxFilter_cl_len,
-                    carp::make_vector<std::string>("boxFilter_C1_D0"),
-                    "   -D anX=" + carp::cast<std::string>(anchor.x)
-                    + " -D anY=" + carp::cast<std::string>(anchor.y)
-                    + " -D ksX=" + carp::cast<std::string>(ksize.width)
-                    + " -D ksY=" + carp::cast<std::string>(ksize.height)
-                    + " -D " + border_type.first );
-            
-                device["boxFilter_C1_D0"] (
-                    reinterpret_cast<cl_mem>(gpu_gray.data),
-                    reinterpret_cast<cl_mem>(gpu_result.data),
-                    static_cast<cl_float>(alpha),
-                    gpu_gray.offset,
-                    gpu_gray.wholerows,
-                    gpu_gray.wholecols,
-                    static_cast<int>(gpu_gray.step),
-                    gpu_result.offset,
-                    gpu_result.rows,
-                    gpu_result.cols,
-                    static_cast<int>(gpu_result.step)
-                    ).groupsize( { blockSizeX, blockSizeY, 1 }, { globalSizeX, globalSizeY, 1 } );
-                check = gpu_result;
-                auto gpu_end = std::chrono::high_resolution_clock::now();
-                elapsed_time_gpu += carp::microseconds(gpu_end - gpu_start);
-                device.erase("boxFilter_C1_D0");
+                    normalizeAnchor(anchor, ksize);
 
+                    cv::ocl::Context * context = cv::ocl::Context::getContext();
+                    carp::opencl::device device(context);
+                    device.source_compile( filtering_boxFilter_cl, filtering_boxFilter_cl_len, carp::make_vector<std::string>("boxFilter_C1_D0")
+                                         , " -D anX=" + carp::cast<std::string>(anchor.x)
+                                         + " -D anY=" + carp::cast<std::string>(anchor.y)
+                                         + " -D ksX=" + carp::cast<std::string>(ksize.width)
+                                         + " -D ksY=" + carp::cast<std::string>(ksize.height)
+                                         + " -D " + border_type.first
+                                         );
+                    auto gpu_start = std::chrono::high_resolution_clock::now();
+                    cv::ocl::oclMat gpu_filtered( cpu_gray.rows, cpu_gray.cols, CV_8U );
+                    cv::ocl::oclMat gpu_gray(cpu_gray);
+                    device["boxFilter_C1_D0"]( reinterpret_cast<cl_mem>(gpu_gray.data), reinterpret_cast<cl_mem>(gpu_filtered.data), static_cast<cl_float>(alpha)
+                                             , gpu_gray.offset, gpu_gray.wholerows, gpu_gray.wholecols
+                                             , static_cast<int>(gpu_gray.step)
+                                             , gpu_filtered.offset, gpu_filtered.rows, gpu_filtered.cols, static_cast<int>(gpu_filtered.step)
+                                             ).groupsize( { blockSizeX, blockSizeY, 1 }, { globalSizeX, globalSizeY, 1 } );
+                    gpu_result = gpu_filtered;
+                    auto gpu_end = std::chrono::high_resolution_clock::now();
+                    elapsed_time_gpu += (gpu_end - gpu_start);
+                    device.erase("boxFilter_C1_D0");
+                }
                 // Verifying the results
-                if ( cv::norm(check - host_filtered) > 0.01 ) {
-                    PRINT(cv::norm(check - host_filtered));                    
-                    cv::imwrite( "gpu_boxFilter_img.png", check );
-                    cv::imwrite( "cpu_boxFilter_img.png", host_filtered );
-                    throw std::runtime_error("The GPU results are not equivalent with the CPU results.");                
+                if ( cv::norm(gpu_result - cpu_result) > 0.01 ) {
+                    PRINT(cv::norm(gpu_result - cpu_result));
+                    cv::imwrite( "gpu_boxFilter_img.png", gpu_result );
+                    cv::imwrite( "cpu_boxFilter_img.png", cpu_result );
+                    throw std::runtime_error("The GPU results are not equivalent with the CPU results.");
                 }
 
             } // for border_type
 
-            timing.print( "boxFilter", elapsed_time_cpu, elapsed_time_gpu );
-            
-            if (elapsed_time_gpu > 1)
-                sum_quotient += static_cast<double>(elapsed_time_cpu) / elapsed_time_gpu;
-
+            timing.print( "boxFilter", elapsed_time_cpu, elapsed_time_gpu, elapsed_time_pencil );
         } // for size
-        
     } // for item
 } // text_boxFilter
 
 
 int main(int argc, char* argv[])
 {
-
-    std::cout << "This executable is iterating over all the files which are present in the directory `./pool'. " << std::endl;    
+    std::cout << "This executable is iterating over all the files which are present in the directory `./pool'. " << std::endl;
 
     auto pool = carp::get_pool("pool");
+    time_boxFilter( pool );
 
-    // Initializing OpenCL
-    cv::ocl::Context * context = cv::ocl::Context::getContext();
-    carp::opencl::device device(context);
-    time_boxFilter( device, pool );
-
-    return EXIT_SUCCESS;    
+    return EXIT_SUCCESS;
 } // main
--- cvIntegral/test_integral.cpp
+++ cvIntegral/test_integral.cpp
@@ -8,101 +8,68 @@
 #include "opencl.hpp"
 #include "utility.hpp"
 #include "imgproc_integral_sum.clh"
-    
+
 
 template<class T0>
 void
-time_integral( carp::opencl::device & device, T0 & pool, int iteration )
+time_integral( T0 & pool, int iteration )
 {
-    carp::TimingShort timing;
+    carp::TimingLong timing;
 
     for ( int q=0; q<iteration; q++ ) {
-        
+
         for ( auto & item : pool ) {
             PRINT(item.path());
-
-            long int elapsed_time_gpu = 0;
-            long int elapsed_time_cpu = 0;
-        
-            cv::Mat host_integral;
             cv::Mat cpu_gray;
-            cv::Mat check;    
-
             cv::cvtColor( item.cpuimg(), cpu_gray, CV_RGB2GRAY );
             if(cpu_gray.cols * cpu_gray.rows <= 2901 * 2901)
-                cv::resize(cpu_gray, cpu_gray, cv::Size(2901,2901));        
-            auto cpu_start = std::chrono::high_resolution_clock::now();
-            cv::integral( cpu_gray, host_integral, CV_32SC1 );        
-            auto cpu_end = std::chrono::high_resolution_clock::now();
-            elapsed_time_cpu += carp::microseconds(cpu_end - cpu_start);
-        
+                cv::resize(cpu_gray, cpu_gray, cv::Size(2901,2901));
+
+            cv::Mat cpu_result, gpu_result;
+            std::chrono::microseconds elapsed_time_cpu(0), elapsed_time_gpu(0), elapsed_time_pen(99999);
+
             {
+                auto cpu_start = std::chrono::high_resolution_clock::now();
+                cv::integral( cpu_gray, cpu_result, CV_32SC1 );
+                auto cpu_end = std::chrono::high_resolution_clock::now();
+                elapsed_time_cpu += (cpu_end - cpu_start);
+            }
+            {
+                cv::ocl::Context * context = cv::ocl::Context::getContext();
+                carp::opencl::device device(context);
+                device.source_compile( imgproc_integral_sum_cl, imgproc_integral_sum_cl_len
+                                     , carp::make_vector<std::string>("integral_sum_cols_D4", "integral_sum_rows_D4" )
+                                     , " -D DOUBLE_SUPPORT"
+                                     );
+
+                auto gpu_start = std::chrono::high_resolution_clock::now();
+
                 cv::ocl::oclMat src(cpu_gray);
-                cv::ocl::oclMat sum;            
-            
+
                 int vlen = 4;
                 int offset = src.offset / vlen;
                 int pre_invalid = src.offset % vlen;
                 int vcols = (pre_invalid + src.cols + vlen - 1) / vlen;
-        
-                cv::ocl::oclMat t_sum;
-                int w = src.cols + 1, h = src.rows + 1;
-                std::string integral_sum_cols;
-                std::string integral_sum_rows;            
-                // if(src.cols * src.rows <= 2901 * 2901)
-                {
-                    t_sum.create(src.cols, src.rows, CV_32SC1);
-                    sum.create(h, w, CV_32SC1);
-                    integral_sum_rows = "integral_sum_rows_D4";
-                    integral_sum_cols = "integral_sum_cols_D4";
-                }
-                // else
-                // {
-                //     t_sum.create(src.cols, src.rows, CV_32FC1);
-                //     sum.create(h, w, CV_32FC1);
-                //     integral_sum_rows = "integral_sum_rows_D5";
-                //     integral_sum_cols = "integral_sum_cols_D5";                
-                // }
 
-                // {
-                //     t_sum.create(src.cols, src.rows, CV_64FC1);
-                //     sum.create(h, w, CV_64FC1);
-                //     integral_sum_rows = "integral_sum_rows_D6";
-                //     integral_sum_cols = "integral_sum_cols_D6";                
-                // }
+                cv::ocl::oclMat t_sum(src.cols, src.rows, CV_32SC1);
+                cv::ocl::oclMat sum(src.rows + 1, src.cols + 1, CV_32SC1);
                 int sum_offset = sum.offset / vlen;
-                auto gpu_start = std::chrono::high_resolution_clock::now();
-                device[integral_sum_cols](
-                    reinterpret_cast<cl_mem>(src.data),
-                    reinterpret_cast<cl_mem>(t_sum.data),
-                    offset,
-                    pre_invalid,
-                    src.rows,
-                    src.cols,
-                    static_cast<int>(src.step),
-                    static_cast<int>(t_sum.step)
-                    ).groupsize( {256, 1, 1}, {((vcols + 1) / 2) * 256, 1, 1} );
-                device[integral_sum_rows](
-                    reinterpret_cast<cl_mem>(t_sum.data),
-                    reinterpret_cast<cl_mem>(sum.data),
-                    t_sum.rows,
-                    t_sum.cols,
-                    static_cast<int>(t_sum.step),
-                    static_cast<int>(sum.step),
-                    sum_offset
-                    ).groupsize( {256, 1, 1}, {t_sum.cols  * 32, 1, 1} );
-
+                device["integral_sum_cols_D4"]( reinterpret_cast<cl_mem>(src.data), reinterpret_cast<cl_mem>(t_sum.data)
+                                              , offset, pre_invalid, src.rows, src.cols, static_cast<int>(src.step), static_cast<int>(t_sum.step)
+                                              ).groupsize( {256, 1, 1}, {((vcols + 1) / 2) * 256, 1, 1} );
+                device["integral_sum_rows_D4"]( reinterpret_cast<cl_mem>(t_sum.data), reinterpret_cast<cl_mem>(sum.data)
+                                              , t_sum.rows, t_sum.cols, static_cast<int>(t_sum.step), static_cast<int>(sum.step), sum_offset
+                                              ).groupsize( {256, 1, 1}, {t_sum.cols  * 32, 1, 1} );
+                gpu_result = sum;
                 auto gpu_end = std::chrono::high_resolution_clock::now();
-                elapsed_time_gpu += carp::microseconds(gpu_end - gpu_start);
-
-                check = sum;
+                elapsed_time_gpu += (gpu_end - gpu_start);
             }
-        
+
             // Verifying the results
-            if ( cv::norm(host_integral - check) > 0.01 ) {
-                PRINT(cv::norm(check - host_integral));
+            if ( cv::norm(cpu_result - gpu_result) > 0.01 ) {
+                PRINT(cv::norm(gpu_result - cpu_result));
                 // no use to write out the results, as they are in float
-                throw std::runtime_error("The GPU results are not equivalent with the CPU results.");                
+                throw std::runtime_error("The GPU results are not equivalent with the CPU results.");
             }
 
             timing.print( "integral image", elapsed_time_cpu, elapsed_time_gpu );
@@ -113,16 +80,11 @@ time_integral( carp::opencl::device & device, T0 & pool, int iteration )
 int main(int argc, char* argv[])
 {
 
-    std::cout << "This executable is iterating over all the files which are present in the directory `./pool'. " << std::endl;    
+    std::cout << "This executable is iterating over all the files which are present in the directory `./pool'. " << std::endl;
 
     auto pool = carp::get_pool("pool");
 
     // Initializing OpenCL
-    cv::ocl::Context * context = cv::ocl::Context::getContext();
-    carp::opencl::device device(context);
-    device.source_compile( imgproc_integral_sum_cl, imgproc_integral_sum_cl_len,
-                           carp::make_vector<std::string>("integral_sum_cols_D4", "integral_sum_rows_D4", "integral_sum_cols_D5", "integral_sum_rows_D5" ),
-                           " -D DOUBLE_SUPPORT" );
-    time_integral( device, pool, 10 );
-    return EXIT_SUCCESS;    
+    time_integral( pool, 10 );
+    return EXIT_SUCCESS;
 }
