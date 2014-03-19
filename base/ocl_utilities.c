771dc1472a7883d53bd8ad26032d0282cbdea794
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2014-03-19 11:48:20 +0100

    Update base/ocl_utilities.c
    Former-commit-id: d74e31e361cc005aa7b014f662e3d2fbd0746dc0

--- base/ocl_utilities.c
+++ base/ocl_utilities.c
@@ -7,84 +7,6 @@
 #include <CL/opencl.h>
 #endif
 
-/* Find a GPU or a CPU associated with the first available platform.
- * This function tries first to look for a GPU in the first available platform,
- * if it fails, it tries to use CPU.
- */
-cl_device_id opencl_create_device()
-{
-	cl_platform_id platform;
-	cl_device_id dev;
-	int err;
-
-	err = clGetPlatformIDs(1, &platform, NULL);
-	if (err < 0) {
-		fprintf(stderr, "Could not find a platform.\n");
-		exit(1);
-	}
-
-	err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev, NULL);
-	if (err == CL_DEVICE_NOT_FOUND)
-		err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev,
-				NULL);
-	if (err < 0) {
-		fprintf(stderr, "Error while looking for a device.\n");
-		exit(1);
-	}
-	return dev;
-}
-
-/* Create an OpenCL program from a source file and compile it.
- */
-cl_program opencl_build_program(cl_context ctx, cl_device_id dev,
-	const char* filename, const char* opencl_options)
-{
-	cl_program program;
-	FILE *program_file;
-	char *program_source, *program_log;
-	size_t program_size, log_size, read;
-	int err;
-
-	program_file = fopen(filename, "r");
-	if (program_file == NULL) {
-		fprintf(stderr, "Could not find the source file.\n");
-		exit(1);
-	}
-	fseek(program_file, 0, SEEK_END);
-	program_size = ftell(program_file);
-	rewind(program_file);
-	program_source = (char *) malloc(program_size + 1);
-	program_source[program_size] = '\0';
-	read = fread(program_source, sizeof(char), program_size, program_file);
-	if (read != program_size) {
-		fprintf(stderr, "Error while reading the kernel.\n");
-		exit(1);
-	}
-	fclose(program_file);
-
-	program = clCreateProgramWithSource(ctx, 1,
-		(const char **)&program_source, &program_size, &err);
-	if (err < 0) {
-		fprintf(stderr, "Could not create the program\n");
-		exit(1);
-	}
-	free(program_source);
-	err = clBuildProgram(program, 0, NULL, opencl_options, NULL, NULL);
-	if (err < 0) {
-		fprintf(stderr, "Could not build the program.\n");
-		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 0,
-				NULL, &log_size);
-		program_log = (char *) malloc(log_size + 1);
-		program_log[log_size] = '\0';
-		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
-				log_size + 1, program_log, NULL);
-		fprintf(stderr, "%s\n", program_log);
-		free(program_log);
-		exit(1);
-	}
-	return program;
-}
-
 /* Return the OpenCL error string for a given error number.
  */
 const char *opencl_error_string(cl_int error)
@@ -154,4 +76,103 @@ const char *opencl_error_string(cl_int error)
 
 	return (index >= 0 && index < errorCount) ?
 		errorString[index] : "Unspecified Error";
+}
+
+/* <sys/time.h> needed for time measurements.  */
+#include <sys/time.h>
+
+/* Find a GPU or a CPU associated with the first available platform.
+ * If use_gpu is set, then this function first tries to look for a GPU
+ * in the first available platform.
+ * If this fails or if use_gpu is not set, then it tries to use the CPU.
+ */
+cl_device_id opencl_create_device(int use_gpu)
+{
+	cl_platform_id platform;
+	cl_device_id dev;
+	int err;
+
+	err = clGetPlatformIDs(1, &platform, NULL);
+	if (err < 0) {
+		fprintf(stderr, "Error %s while looking for a platform.\n",
+				opencl_error_string(err));
+		exit(1);
+	}
+
+	err = CL_DEVICE_NOT_FOUND;
+	if (use_gpu)
+		err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &dev,
+				NULL);
+	if (err == CL_DEVICE_NOT_FOUND)
+		err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 1, &dev,
+				NULL);
+	if (err < 0) {
+		fprintf(stderr, "Error %s while looking for a device.\n",
+				opencl_error_string(err));
+		exit(1);
+	}
+	return dev;
+}
+
+/* Create an OpenCL program from a source file and compile it.
+ */
+cl_program opencl_build_program(cl_context ctx, cl_device_id dev,
+	const char* filename, const char* opencl_options)
+{
+	cl_program program;
+	FILE *program_file;
+	char *program_source, *program_log;
+	size_t program_size, log_size, read;
+	int err;
+
+	program_file = fopen(filename, "r");
+	if (program_file == NULL) {
+		fprintf(stderr, "Could not find the source file.\n");
+		exit(1);
+	}
+	fseek(program_file, 0, SEEK_END);
+	program_size = ftell(program_file);
+	rewind(program_file);
+	program_source = (char *) malloc(program_size + 1);
+	program_source[program_size] = '\0';
+	read = fread(program_source, sizeof(char), program_size, program_file);
+	if (read != program_size) {
+		fprintf(stderr, "Error while reading the kernel.\n");
+		exit(1);
+	}
+	fclose(program_file);
+
+	program = clCreateProgramWithSource(ctx, 1,
+		(const char **)&program_source, &program_size, &err);
+	if (err < 0) {
+		fprintf(stderr, "Could not create the program\n");
+		exit(1);
+	}
+	free(program_source);
+	err = clBuildProgram(program, 0, NULL, opencl_options, NULL, NULL);
+	if (err < 0) {
+		fprintf(stderr, "Could not build the program.\n");
+		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 0,
+				NULL, &log_size);
+		program_log = (char *) malloc(log_size + 1);
+		program_log[log_size] = '\0';
+		clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG,
+				log_size + 1, program_log, NULL);
+		fprintf(stderr, "%s\n", program_log);
+		free(program_log);
+		exit(1);
+	}
+	return program;
+}
+
+/* Return current time (number of seconds since the Epoch).  */
+double ppcg_get_time()
+{
+	struct timeval Tp;
+	int stat;
+
+	stat = gettimeofday (&Tp, NULL);
+	if (stat != 0)
+		fprintf(stderr, "Error return from gettimeofday: %d", stat);
+	return Tp.tv_sec + Tp.tv_usec * 1.0e-6;
 }
