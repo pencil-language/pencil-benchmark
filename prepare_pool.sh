2d4b402ba4f106981b6f5450bbb78f1bdc3bb89e
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2014-04-10 15:20:08 +0200

    prepare_pool.sh: update the script
    Former-commit-id: aaff05cd9bb2791502ab6ba6c66ece9ff3c47e07

--- prepare_pool.sh
+++ prepare_pool.sh
@@ -1,6 +1,8 @@
 cd build/pool
-rm *.jpg
-rm *.xml
-7z x Kings_Cross_Western_Concourse_-_central_position_-_2012-05-02.75.jpg.7z
-7z x response_dumps.xml.7z
+echo "Removing *.jpg and *.xml files in build/pool"
+rm -rf *.jpg
+rm -rf *.xml
+echo "Now extracting 1 image file and 1 xml file for tests"
+7z x Kings_Cross_Western_Concourse_-_central_position_-_2012-05-02.75.jpg.7z >> /dev/null
+7z x response_dumps.xml.7z >> /dev/null
 cd -
