ea340e40e206dad0d22272356f6d1899c8606778
Riyadh BAGHDADI <baghdadi.mr@gmail.com>
2014-05-20 17:58:51 +0200

    hog.pencil.c inline hog() function
    Former-commit-id: db55cb74cf62f2f640454a0fa4f6ebe255ddcb3a

--- hog/hog.pencil.c
+++ hog/hog.pencil.c
@@ -39,124 +39,6 @@ static void normalize(float hist[NUMBER_OF_CELLS][NUMBER_OF_CELLS][NUMBER_OF_BIN
                 hist[i][j][k] = hist[i][j][k] * scale;
 }
 
-static void hog( const int rows
-               , const int cols
-               , const int step
-               , const uint8_t image[static const restrict rows][step]
-               , const float location_x
-               , const float location_y
-               , const float block_size
-               , float hist[static const restrict NUMBER_OF_CELLS][NUMBER_OF_CELLS][NUMBER_OF_BINS]    //out
-               )
-{
-    const float cell_size = block_size / NUMBER_OF_CELLS;
-    const float minx = location_x - block_size / 2.0f;
-    const float miny = location_y - block_size / 2.0f;
-    const float maxx = location_x + block_size / 2.0f;
-    const float maxy = location_y + block_size / 2.0f;
-
-    const int minxi = max(ceil(minx), 1);
-    const int minyi = max(ceil(miny), 1);
-    const int maxxi = min(ceil(maxx), cols - 2);
-    const int maxyi = min(ceil(maxy), rows - 2);
-
-    memset(hist, 0, HISTOGRAM_BINS);
-
-#if GAUSSIAN_WEIGHTS
-    const float sigma = block_size / 2.0f;
-    const float sigmaSq = sigma*sigma;
-    const float m1p2sigmaSq = -1.0f / (2.0f * sigmaSq);
-#endif
-
-#pragma scop
-
-    int cellyi;
-    int cellxi;
-
-#if __PENCIL__
-    __pencil_assume(cellxi < NUMBER_OF_CELLS);
-    __pencil_assume(cellyi < NUMBER_OF_CELLS);
-    __pencil_assume(cellxi >= 0);
-    __pencil_assume(cellyi >= 0);
-#endif
-
-#pragma pencil independent reduction(+:hist)
-    for (int pointy = minyi; pointy <= maxyi; ++pointy) {
-#if SPARTIAL_WEIGHTS
-        float relative_pos_y = (pointy - miny) / cell_size - 0.5f;
-        cellyi = floor(relative_pos_y);
-        float yscale1 = relative_pos_y - cellyi;
-        float yscale0 = 1.0f - yscale1;
-#endif
-#if GAUSSIAN_WEIGHTS
-        float dy = pointy - location_y;
-        float dySq = dy*dy;
-#endif
-#pragma pencil independent reduction(+:hist)
-        for (int pointx = minxi; pointx <= maxxi; ++pointx) {
-#if SPARTIAL_WEIGHTS
-            float relative_pos_x = (pointx - minx) / cell_size - 0.5f;
-            cellxi = floor(relative_pos_x);
-            float xscale1 = relative_pos_x - cellxi;
-            float xscale0 = 1.0f - xscale1;
-#endif
-
-#if GAUSSIAN_WEIGHTS
-            float dx = pointx - location_x;
-            float dxSq = dx*dx;
-#endif
-            float mdx = image[pointy][pointx+1] - image[pointy][pointx-1];
-            float mdy = image[pointy+1][pointx] - image[pointy-1][pointx];
-
-            float magnitude = hypot(mdx, mdy);   //or = sqrt(mdx*mdx + mdy*mdy);
-#if SIGNED_HOG
-            float orientation = atan2(mdy, mdx) / M_PI * 180.0f;
-#else
-            float orientation = tan2(mdy / mdx + DBL_EPSILON) / M_PI * 180.0f + 90.0f;
-#endif
-#if GAUSSIAN_WEIGHTS
-            magnitude *= exp((dxSq+dySq) * m1p2sigmaSq);
-#endif
-            float relative_orientation = (orientation - BINSIZE_IN_DEGREES/2.0) / BINSIZE_IN_DEGREES;
-            int bin1 = ceil(relative_orientation);
-            int bin0 = bin1 - 1;
-            float bin_weight0 = magnitude * (bin1 - relative_orientation);
-            float bin_weight1 = magnitude * (relative_orientation - bin0);
-            bin0 = (bin0 + NUMBER_OF_BINS) % NUMBER_OF_BINS;
-            bin1 = (bin1 + NUMBER_OF_BINS) % NUMBER_OF_BINS;
-
-#if SPARTIAL_WEIGHTS
-            if (cellyi >= 0 && cellxi >= 0) {
-                hist[cellyi][cellxi][bin0] += yscale0 * xscale0 * bin_weight0;
-                hist[cellyi][cellxi][bin1] += yscale0 * xscale0 * bin_weight1;
-            }
-            if (cellyi >= 0 && cellxi < NUMBER_OF_CELLS - 1) {
-                hist[cellyi][cellxi+1][bin0] += yscale0 * xscale1 * bin_weight0;
-                hist[cellyi][cellxi+1][bin1] += yscale0 * xscale1 * bin_weight1;
-            }
-            if (cellyi < NUMBER_OF_CELLS - 1 && cellxi >= 0) {
-                hist[cellyi+1][cellxi][bin0] += yscale1 * xscale0 * bin_weight0;
-                hist[cellyi+1][cellxi][bin1] += yscale1 * xscale0 * bin_weight1;
-            }
-            if (cellyi < NUMBER_OF_CELLS - 1 && cellxi < NUMBER_OF_CELLS - 1) {
-                hist[cellyi+1][cellxi+1][bin0] += yscale1 * xscale1 * bin_weight0;
-                hist[cellyi+1][cellxi+1][bin1] += yscale1 * xscale1 * bin_weight1;
-            }
-#elif NUMBER_OF_CELLS == 1
-            hist[0][0][bin0] += bin_weight0;
-            hist[0][0][bin1] += bin_weight1;
-#else
-            cellxi = floor((pointx - minx) / cell_size);
-            cellyi = floor((pointy - miny) / cell_size);
-            hist[cellxi][cellyi][bin0] += bin_weight0;
-            hist[cellxi][cellyi][bin1] += bin_weight1;
-#endif
-        }
-    }
-#pragma endscop
-    normalize(hist);
-}
-
 static void hog_multi( const int rows
                      , const int cols
                      , const int step
@@ -167,10 +49,121 @@ static void hog_multi( const int rows
                      , const float block_size
                      , float hist[static const restrict num_locations][NUMBER_OF_CELLS][NUMBER_OF_CELLS][NUMBER_OF_BINS]    //out
                      ) {
+#pragma scop
+
+
 #pragma pencil independent
     for (int i = 0; i < num_locations; ++i) {
-        hog(rows, cols, step, image, location_x[i], location_y[i], block_size, hist);
+	    const float cell_size = block_size / NUMBER_OF_CELLS;
+	    const float minx = location_x[i] - block_size / 2.0f;
+	    const float miny = location_y[i] - block_size / 2.0f;
+	    const float maxx = location_x[i] + block_size / 2.0f;
+	    const float maxy = location_y[i] + block_size / 2.0f;
+
+	    const int minxi = max(ceil(minx), 1);
+	    const int minyi = max(ceil(miny), 1);
+	    const int maxxi = min(ceil(maxx), cols - 2);
+	    const int maxyi = min(ceil(maxy), rows - 2);
+
+	    //	    memset(hist[i], 0, HISTOGRAM_BINS);
+	    for (int k = 0; k < HISTOGRAM_BINS; k++)
+		    hist[i][0][0][k] = 0;
+
+
+#if GAUSSIAN_WEIGHTS
+	    const float sigma = block_size / 2.0f;
+	    const float sigmaSq = sigma*sigma;
+	    const float m1p2sigmaSq = -1.0f / (2.0f * sigmaSq);
+#endif
+
+	    int cellyi;
+	    int cellxi;
+
+	    #pragma pencil independent reduction(+:hist[i])
+	    for (int pointy = minyi; pointy <= maxyi; ++pointy) {
+#if SPARTIAL_WEIGHTS
+		float relative_pos_y = (pointy - miny) / cell_size - 0.5f;
+		cellyi = floor(relative_pos_y);
+		float yscale1 = relative_pos_y - cellyi;
+		float yscale0 = 1.0f - yscale1;
+#endif
+#if GAUSSIAN_WEIGHTS
+		float dy = pointy - location_y[i];
+		float dySq = dy*dy;
+#endif
+
+#if __PENCIL__
+    __pencil_assume(cellxi < NUMBER_OF_CELLS);
+    __pencil_assume(cellyi < NUMBER_OF_CELLS);
+    __pencil_assume(cellxi >= 0);
+    __pencil_assume(cellyi >= 0);
+#endif
+
+		#pragma pencil independent reduction(+:hist[i])
+		for (int pointx = minxi; pointx <= maxxi; ++pointx) {
+#if SPARTIAL_WEIGHTS
+		    float relative_pos_x = (pointx - minx) / cell_size - 0.5f;
+		    cellxi = floor(relative_pos_x);
+		    float xscale1 = relative_pos_x - cellxi;
+		    float xscale0 = 1.0f - xscale1;
+#endif
+
+#if GAUSSIAN_WEIGHTS
+		    float dx = pointx - location_x[i];
+		    float dxSq = dx*dx;
+#endif
+		    float mdx = image[pointy][pointx+1] - image[pointy][pointx-1];
+		    float mdy = image[pointy+1][pointx] - image[pointy-1][pointx];
+
+		    float magnitude = hypot(mdx, mdy);   //or = sqrt(mdx*mdx + mdy*mdy);
+#if SIGNED_HOG
+		    float orientation = atan2(mdy, mdx) / M_PI * 180.0f;
+#else
+		    float orientation = tan2(mdy / mdx + DBL_EPSILON) / M_PI * 180.0f + 90.0f;
+#endif
+#if GAUSSIAN_WEIGHTS
+		    magnitude *= exp((dxSq+dySq) * m1p2sigmaSq);
+#endif
+		    float relative_orientation = (orientation - BINSIZE_IN_DEGREES/2.0) / BINSIZE_IN_DEGREES;
+		    int bin1 = ceil(relative_orientation);
+		    int bin0 = bin1 - 1;
+		    float bin_weight0 = magnitude * (bin1 - relative_orientation);
+		    float bin_weight1 = magnitude * (relative_orientation - bin0);
+		    bin0 = (bin0 + NUMBER_OF_BINS) % NUMBER_OF_BINS;
+		    bin1 = (bin1 + NUMBER_OF_BINS) % NUMBER_OF_BINS;
+
+#if SPARTIAL_WEIGHTS
+		    if (cellyi >= 0 && cellxi >= 0) {
+			hist[i][cellyi][cellxi][bin0] += yscale0 * xscale0 * bin_weight0;
+			hist[i][cellyi][cellxi][bin1] += yscale0 * xscale0 * bin_weight1;
+		    }
+		    if (cellyi >= 0 && cellxi < NUMBER_OF_CELLS - 1) {
+			hist[i][cellyi][cellxi+1][bin0] += yscale0 * xscale1 * bin_weight0;
+			hist[i][cellyi][cellxi+1][bin1] += yscale0 * xscale1 * bin_weight1;
+		    }
+		    if (cellyi < NUMBER_OF_CELLS - 1 && cellxi >= 0) {
+			hist[i][cellyi+1][cellxi][bin0] += yscale1 * xscale0 * bin_weight0;
+			hist[i][cellyi+1][cellxi][bin1] += yscale1 * xscale0 * bin_weight1;
+		    }
+		    if (cellyi < NUMBER_OF_CELLS - 1 && cellxi < NUMBER_OF_CELLS - 1) {
+			hist[i][cellyi+1][cellxi+1][bin0] += yscale1 * xscale1 * bin_weight0;
+			hist[i][cellyi+1][cellxi+1][bin1] += yscale1 * xscale1 * bin_weight1;
+		    }
+#elif NUMBER_OF_CELLS == 1
+		    hist[i][0][0][bin0] += bin_weight0;
+		    hist[i][0][0][bin1] += bin_weight1;
+#else
+		    cellxi = floor((pointx - minx) / cell_size);
+		    cellyi = floor((pointy - miny) / cell_size);
+		    hist[i][cellxi][cellyi][bin0] += bin_weight0;
+		    hist[i][cellxi][cellyi][bin1] += bin_weight1;
+#endif
+		}
+	    }
+	    normalize(hist[i]);
     }
+#pragma endscop
+
 }
 
 void pencil_hog( const int rows
