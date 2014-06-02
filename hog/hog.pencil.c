0e3bb43fa1aca0bc7d7563b397b629becb958f20
Baghdadi <baghdadi.mr@gmail.com>
2014-06-02 19:57:28 +0200

    hog.pencil.c modify the names of some variables to avoid having the same names as the names of variables generated automatically by ppcg
    Former-commit-id: 0ac8f65167cb7b744a3e005a25821a0fe191885d

--- hog/hog.pencil.c
+++ hog/hog.pencil.c
@@ -23,18 +23,18 @@ static void hog_multi( const int rows
                      , const int num_locations
                      , const float location_x[static const restrict num_locations]
                      , const float location_y[static const restrict num_locations]
-                     , const float block_size
+                     , const float blck_size
                      , float hist[static const restrict num_locations][NUMBER_OF_CELLS][NUMBER_OF_CELLS][NUMBER_OF_BINS]    //out
                      ) {
 #pragma scop
 
 #pragma pencil independent
     for (int i = 0; i < num_locations; ++i) {
-	    float cell_size = block_size / NUMBER_OF_CELLS;
-	    float minx = location_x[i] - block_size / 2.0f;
-	    float miny = location_y[i] - block_size / 2.0f;
-	    float maxx = location_x[i] + block_size / 2.0f;
-	    float maxy = location_y[i] + block_size / 2.0f;
+	    float cell_size = blck_size / NUMBER_OF_CELLS;
+	    float minx = location_x[i] - blck_size / 2.0f;
+	    float miny = location_y[i] - blck_size / 2.0f;
+	    float maxx = location_x[i] + blck_size / 2.0f;
+	    float maxy = location_y[i] + blck_size / 2.0f;
 
 	    int minxi = max(ceil(minx), 1);
 	    int minyi = max(ceil(miny), 1);
@@ -47,7 +47,7 @@ static void hog_multi( const int rows
 	          hist[i][j1][j2][j3] = 0;
 
 #if GAUSSIAN_WEIGHTS
-	    const float sigma = block_size / 2.0f;
+	    const float sigma = blck_size / 2.0f;
 	    const float sigmaSq = sigma*sigma;
 	    const float m1p2sigmaSq = -1.0f / (2.0f * sigmaSq);
 #endif
@@ -78,10 +78,10 @@ static void hog_multi( const int rows
                 float dx = pointx - location_x[i];
                 float dxSq = dx*dx;
 #endif
-		int t1 = pointx-1;
-		int t2 = pointy-1;
-                float mdx = image[pointy][pointx+1] - image[pointy][t1];
-                float mdy = image[pointy+1][pointx] - image[t2][pointx];
+		int temp1 = pointx-1;
+		int temp2 = pointy-1;
+                float mdx = image[pointy][pointx+1] - image[pointy][temp1];
+                float mdy = image[pointy+1][pointx] - image[temp2][pointx];
 
                 float magnitude = hypot(mdx, mdy);   //or = sqrt(mdx*mdx + mdy*mdy);
 #if SIGNED_HOG
@@ -150,9 +150,9 @@ void pencil_hog( const int rows
                , const int num_locations
                , const float location_x[]
                , const float location_y[]
-               , const float block_size
+               , const float blck_size
                , float hist[]    //out
                ) {
-    hog_multi(rows,cols,step,image,num_locations,location_x,location_y,block_size,hist);
+    hog_multi(rows,cols,step,image,num_locations,location_x,location_y,blck_size,hist);
 }
 
