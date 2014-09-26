4416f3944bb5668864bc8e78370be693f7d18c5e
Robert David <robert.david@realeyesit.com>
2014-09-26 16:03:33 +0200

    Histogram: Add scop and __pencil_assume calls.
    Former-commit-id: 202161d1c34f4687d68c2e3f863b2a48a2013a4d

--- histogram/histogram.pencil.c
+++ histogram/histogram.pencil.c
@@ -1,16 +1,22 @@
 #include "histogram.pencil.h"
+#include <pencil.h>
 
-void pencil_calcHist( const int rows
+static void calcHist( const int rows
                     , const int cols
                     , const int step
-                    , const uint8_t image[]
-                    , int hist[HISTOGRAM_BINS]    //out
+                    , const uint8_t image[static const restrict rows][step]
+                    , int hist[static const restrict HISTOGRAM_BINS]    //out
                     )
 {
+#pragma scop
+    __pencil_assume(rows >  0);
+    __pencil_assume(cols >  0);
+    __pencil_assume(step >= cols);
+
     #pragma pencil independent
     for(int b = 0; b < HISTOGRAM_BINS; ++b)
         hist[b] = 0;
-    
+
     #pragma pencil independent reduction(+:hist)
     for(int r = 0; r < rows; ++r)
     {
@@ -21,4 +27,10 @@ void pencil_calcHist( const int rows
             ++hist[pixel];
         }
     }
+#pragma endscop
+}
+
+void pencil_calcHist( const int rows, const int cols, const int step, const uint8_t image[], int hist[HISTOGRAM_BINS])
+{
+    calcHist( rows, cols, step, (const uint8_t(*)[step])image, hist);
 }
