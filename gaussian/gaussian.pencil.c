bb404d4c298503b5e5a1a7ffaa421b7b247bc711
Robert David <robert.david@realeyesit.com>
2014-09-26 13:06:29 +0200

    Simplify Gaussian's pencil implementation
    Reflect that the two kernels are 1D arrays, not 2D.
    
    
    Former-commit-id: 11d701b55bbc34ba810f95941f040de78f669295

--- gaussian/gaussian.pencil.c
+++ gaussian/gaussian.pencil.c
@@ -1,5 +1,6 @@
 #include "gaussian.pencil.h"
 #include <pencil.h>
+#include <assert.h>
 
 #if !__PENCIL__
 #include <stdlib.h>
@@ -9,14 +10,10 @@ static void gaussian( const int rows
                     , const int cols
                     , const int step
                     , const float src[static const restrict rows][step]
-                    , const int kernelX_rows
-                    , const int kernelX_cols
-                    , const int kernelX_step
-                    , const float kernelX[static const restrict kernelX_rows][kernelX_step]
-                    , const int kernelY_rows
-                    , const int kernelY_cols
-                    , const int kernelY_step
-                    , const float kernelY[static const restrict kernelY_rows][kernelY_step]
+                    , const int kernelX_length
+                    , const float kernelX[static const restrict kernelX_length]
+                    , const int kernelY_length
+                    , const float kernelY[static const restrict kernelY_length]
                     , float conv[static const restrict rows][step]
                     )
 {
@@ -24,16 +21,10 @@ static void gaussian( const int rows
     __pencil_assume(rows         >  0);
     __pencil_assume(cols         >  0);
     __pencil_assume(step         >= cols);
-    __pencil_assume(kernelX_rows >  0);
-    __pencil_assume(kernelX_cols >  0);
-    __pencil_assume(kernelX_step >= kernelX_cols);
-    __pencil_assume(kernelY_rows >  0);
-    __pencil_assume(kernelY_cols >  0);
-    __pencil_assume(kernelY_step >= kernelY_cols);
-    __pencil_assume(kernelX_rows <= 2);
-    __pencil_assume(kernelX_cols <= 128);
-    __pencil_assume(kernelY_rows <= 128);
-    __pencil_assume(kernelY_cols <= 2);
+    __pencil_assume(kernelX_length >  0);
+    __pencil_assume(kernelX_length <= 128);
+    __pencil_assume(kernelY_length >  0);
+    __pencil_assume(kernelY_length <= 128);
     {
 #if __PENCIL__
         float temp[rows][step];
@@ -48,14 +39,11 @@ static void gaussian( const int rows
             {
                 float prod = 0.;
                 #pragma pencil independent reduction (+: prod);
-                for ( int e = 0; e < kernelX_rows; e++ )
+                for ( int r = 0; r < kernelX_length; r++ )
                 {
-                    for ( int r = 0; r < kernelX_cols; r++ )
-                    {
-                        int row = clamp(q + e - kernelX_rows / 2, 0, rows-1);
-                        int col = clamp(w + r - kernelX_cols / 2, 0, cols-1);
-                        prod += src[row][col] * kernelX[e][r];
-                    }
+                    int row = q;
+                    int col = clamp(w + r - kernelX_length / 2, 0, cols-1);
+                    prod += src[row][col] * kernelX[r];
                 }
                 temp[q][w] = prod;
             }
@@ -68,14 +56,11 @@ static void gaussian( const int rows
             {
                 float prod = 0.;
                 #pragma pencil independent reduction (+: prod);
-                for ( int e = 0; e < kernelY_rows; e++ )
+                for ( int e = 0; e < kernelY_length; e++ )
                 {
-                    for ( int r = 0; r < kernelY_cols; r++ )
-                    {
-                        int row = clamp(q + e - kernelY_rows / 2, 0, rows-1);
-                        int col = clamp(w + r - kernelY_cols / 2, 0, cols-1);
-                        prod += temp[row][col] * kernelY[e][r];
-                    }
+                    int row = clamp(q + e - kernelY_length / 2, 0, rows-1);
+                    int col = w;
+                    prod += temp[row][col] * kernelY[e];
                 }
                 conv[q][w] = prod;
             }
@@ -102,9 +87,16 @@ void pencil_gaussian( const int rows
                     , float conv[]
                     )
 {
-    gaussian(         rows,         cols,         step, (const float(*)[        step])src
-            , kernelX_rows, kernelX_cols, kernelX_step, (const float(*)[kernelX_step])kernelX
-            , kernelY_rows, kernelY_cols, kernelY_step, (const float(*)[kernelY_step])kernelY
-            ,                                           (      float(*)[        step])conv
+//    printf("%d %d %d / %d %d %d\n", kernelX_rows, kernelX_cols, kernelX_step, kernelY_rows, kernelY_cols, kernelY_step);
+    assert( kernelX_rows == 1 );
+    assert( kernelX_cols  > 0 );
+    assert( kernelX_step == kernelX_cols );
+    assert( kernelY_rows  > 0 );
+    assert( kernelY_cols == 1 );
+    assert( kernelY_step == 1 );
+    gaussian( rows, cols, step, (const float(*)[step])src
+            , kernelX_cols, kernelX
+            , kernelY_rows, kernelY
+            , (float(*)[step])conv
             );
 }
