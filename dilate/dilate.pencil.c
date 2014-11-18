fc2111b683e0efeb2aff80e4061559694b6a1f69
Robert David <robert.david@realeyesit.com>
2014-11-18 17:59:15 +0100

    Dilate: add assumes and pragmas
    Former-commit-id: 7aef88fb1bd32802f14908a4845a10bfe762c3cc

--- dilate/dilate.pencil.c
+++ dilate/dilate.pencil.c
@@ -17,8 +17,13 @@ static void dilate( const int rows
                   )
 {
 #pragma scop
+    __pencil_assume(rows       >  0);
+    __pencil_assume(cols       >  0);
+    __pencil_assume(step       >= cols);
+    __pencil_assume(dilate_step>= cols);
     __pencil_assume(se_rows    <= 9);
     __pencil_assume(se_cols    <= 9);
+    __pencil_assume(se_step    >= se_cols);
     __pencil_assume(anchor_row >= 0);
     __pencil_assume(anchor_row < se_rows);
     __pencil_assume(anchor_col >= 0);
@@ -33,12 +38,13 @@ static void dilate( const int rows
             #pragma pencil independent reduction(max: sup)
             for ( int e = 0; e < se_rows; e++ )
             {
+                #pragma pencil independent reduction(max: sup)
                 for ( int r = 0; r < se_cols; r++ )
                 {
-                   int candidate_row = clampi(q - anchor_row + e, 0, rows);
-                   int candidate_col = clampi(w - anchor_col + r, 0, cols);
+                    int candidate_row = clampi(q - anchor_row + e, 0, rows);
+                    int candidate_col = clampi(w - anchor_col + r, 0, cols);
 
-		   sup = (se[e][r]!=0) ? max(sup, cpu_gray[candidate_row][candidate_col]) : sup;
+                    sup = (se[e][r]!=0) ? ubmax(sup, cpu_gray[candidate_row][candidate_col]) : sup;
                 }
             }
             dilate[q][w] = sup;
