b912fa4159a8409df66f64f3801f2ad4c5f610de
Robert David <robert.david@realeyesit.com>
2015-03-06 11:16:47 +0100

    Remove old autotuning scripts
    Former-commit-id: 3a9918fedf7b413a53fe34797fd0770e8e368727

--- prepare_pool.sh
+++
@@ -1,8 +0,0 @@
-cd build/pool
-echo
-echo "Removing *.jpg and *.xml files in build/pool"
-rm -rf *.jpg
-rm -rf *.xml
-echo "Now extracting 1 image file and 1 xml file for tests"
-git checkout M104_ngc4594_sombrero_galaxy_hi-res.jpg
-cd -
