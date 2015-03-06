9b4eae9d790f3c931a2f590b053342052cfc47fa
Robert David <robert.david@realeyesit.com>
2015-03-06 11:21:01 +0100

    Rename some variables to avoid a bug.
    Former-commit-id: bba1e83d1c5a564e78f18830c66a5b9aab31b7a2

--- gaussian/gaussian.pencil.c
+++ gaussian/gaussian.pencil.c
@@ -39,15 +39,15 @@ static void gaussian( const int rows
             #pragma pencil independent
             for ( int w = 0; w < cols; w++ )
             {
-                float prod = 0.;
-                #pragma pencil independent reduction (+: prod);
+                float prod1 = 0.;
+                #pragma pencil independent reduction (+: prod1);
                 for ( int r = 0; r < kernelX_length; r++ )
                 {
-                    int row = q;
-                    int col = iclampi(w + r - kernelX_length / 2, 0, cols-1);
-                    prod += src[row][col] * kernelX[r];
+                    int row1 = q;
+                    int col1 = iclampi(w + r - kernelX_length / 2, 0, cols-1);
+                    prod1 += src[row1][col1] * kernelX[r];
                 }
-                temp[q][w] = prod;
+                temp[q][w] = prod1;
             }
         }
         #pragma pencil independent
@@ -56,15 +56,15 @@ static void gaussian( const int rows
             #pragma pencil independent
             for ( int w = 0; w < cols; w++ )
             {
-                float prod = 0.;
-                #pragma pencil independent reduction (+: prod);
+                float prod2 = 0.;
+                #pragma pencil independent reduction (+: prod2);
                 for ( int e = 0; e < kernelY_length; e++ )
                 {
-                    int row = iclampi(q + e - kernelY_length / 2, 0, rows-1);
-                    int col = w;
-                    prod += temp[row][col] * kernelY[e];
+                    int row2 = iclampi(q + e - kernelY_length / 2, 0, rows-1);
+                    int col2 = w;
+                    prod2 += temp[row2][col2] * kernelY[e];
                 }
-                conv[q][w] = prod;
+                conv[q][w] = prod2;
             }
         }
 #if !__PENCIL__
